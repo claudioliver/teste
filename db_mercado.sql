15b22eafbcfbb5363968ecfb0dd686bb03e47569
claudio <claudioliver7@gmail.com>
2023-09-19 11:17:02 -0300

    Update

--- db_mercado.sql
+++ db_mercado.sql
@@ -1,13 +1,17 @@
 create database db_mercado;
 
+CREATE TABLE fornecedores (
+    id_fornecedor serial PRIMARY KEY UNIQUE,
+    nome VARCHAR(30),
+    cnpj VARCHAR(18) 
 
-CREATE TABLE fornecedores (id_fornecedor serial PRIMARY KEY UNIQUE,
-                                                            nome VARCHAR(30),
-                                                                 cnpj VARCHAR(18));
+);
 
-
-CREATE TABLE produtos (id_produto serial PRIMARY KEY UNIQUE,
-                                                     nome VARCHAR(50),
-                                                          marca VARCHAR(30),
-                                                                quantidade INT, volume INT, categoria INT);
-
+CREATE TABLE produtos (
+    id_produto serial PRIMARY KEY UNIQUE,
+    nome VARCHAR(50),
+    marca VARCHAR(30),
+    quantidade INT,
+    volume INT,
+    categoria INT
+);
